61addd51cde72a5c993c9cf29995b7743e301b9b
unknown <kanu.malivad@gmail.com>
2018-09-10 17:33:48 +0530

    changed APP_ID

--- fb-config.php
+++ fb-config.php
@@ -1,7 +1,7 @@
 <?php 
 
-    define('APP_ID', '289696711856954');
-    define('APP_SECRET', '9238ba174c795e725ad7673c16838d43');
+    define('APP_ID', '1784504198335909');
+    define('APP_SECRET', '38070806dcf50aa7a3ef69b91ffbf103');
     define('REDIRECT_URL', 'https://rtcamp-fbchallenge-mk.herokuapp.com/');
 
     define('DOMAIN',"https://rtcamp-fbchallenge-mk.herokuapp.com/");
